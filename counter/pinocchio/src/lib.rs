96671ddfb2eb4c04808688a5ee041a941f14ac7c
ananas-block <jorrit@lightprotocol.com>
2025-06-12 22:45:02 +0100

    feat: add pinocchio counter program

---
+++ counter/pinocchio/src/lib.rs
@@ -0,0 +1,330 @@
+#![allow(unexpected_cfgs)]
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use light_macros::pubkey_array;
+use light_sdk_pinocchio::{
+    account::LightAccount,
+    address::v1::derive_address,
+    cpi::{CpiAccounts, CpiInputs, CpiSigner},
+    derive_light_cpi_signer,
+    error::LightSdkError,
+    instruction::{
+        account_meta::{CompressedAccountMeta, CompressedAccountMetaClose},
+        PackedAddressTreeInfo,
+    },
+    LightDiscriminator, LightHasher, ValidityProof,
+};
+use pinocchio::{
+    account_info::AccountInfo,
+    entrypoint,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+pub const ID: Pubkey = pubkey_array!("GRLu2hKaAiMbxpkAM1HeXzks9YeGuz18SEgXEizVvPqX");
+pub const LIGHT_CPI_SIGNER: CpiSigner =
+    derive_light_cpi_signer!("GRLu2hKaAiMbxpkAM1HeXzks9YeGuz18SEgXEizVvPqX");
+
+entrypoint!(process_instruction);
+
+#[repr(u8)]
+pub enum InstructionType {
+    CreateCounter = 0,
+    IncrementCounter = 1,
+    DecrementCounter = 2,
+    ResetCounter = 3,
+    CloseCounter = 4,
+}
+
+impl TryFrom<u8> for InstructionType {
+    type Error = LightSdkError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(InstructionType::CreateCounter),
+            1 => Ok(InstructionType::IncrementCounter),
+            2 => Ok(InstructionType::DecrementCounter),
+            3 => Ok(InstructionType::ResetCounter),
+            4 => Ok(InstructionType::CloseCounter),
+            _ => panic!("Invalid instruction discriminator."),
+        }
+    }
+}
+
+#[derive(
+    Debug, Default, Clone, BorshSerialize, BorshDeserialize, LightDiscriminator, LightHasher,
+)]
+pub struct CounterAccount {
+    #[hash]
+    pub owner: Pubkey,
+    pub value: u64,
+}
+
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct CreateCounterInstructionData {
+    pub proof: ValidityProof,
+    pub address_tree_info: PackedAddressTreeInfo,
+    pub output_state_tree_index: u8,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct IncrementCounterInstructionData {
+    pub proof: ValidityProof,
+    pub counter_value: u64,
+    pub account_meta: CompressedAccountMeta,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct DecrementCounterInstructionData {
+    pub proof: ValidityProof,
+    pub counter_value: u64,
+    pub account_meta: CompressedAccountMeta,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct ResetCounterInstructionData {
+    pub proof: ValidityProof,
+    pub counter_value: u64,
+    pub account_meta: CompressedAccountMeta,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct CloseCounterInstructionData {
+    pub proof: ValidityProof,
+    pub counter_value: u64,
+    pub account_meta: CompressedAccountMetaClose,
+}
+
+#[derive(Debug, Clone)]
+pub enum CounterError {
+    Unauthorized,
+    Overflow,
+    Underflow,
+}
+
+impl From<CounterError> for ProgramError {
+    fn from(e: CounterError) -> Self {
+        match e {
+            CounterError::Unauthorized => ProgramError::Custom(1),
+            CounterError::Overflow => ProgramError::Custom(2),
+            CounterError::Underflow => ProgramError::Custom(3),
+        }
+    }
+}
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> Result<(), ProgramError> {
+    if instruction_data.is_empty() {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    let discriminator = InstructionType::try_from(instruction_data[0])
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
+
+    match discriminator {
+        InstructionType::CreateCounter => {
+            let instruction_data = CreateCounterInstructionData::try_from_slice(&instruction_data[1..])
+                .map_err(|_| ProgramError::InvalidInstructionData)?;
+            create_counter(accounts, instruction_data)
+        }
+        InstructionType::IncrementCounter => {
+            let instruction_data = IncrementCounterInstructionData::try_from_slice(&instruction_data[1..])
+                .map_err(|_| ProgramError::InvalidInstructionData)?;
+            increment_counter(accounts, instruction_data)
+        }
+        InstructionType::DecrementCounter => {
+            let instruction_data = DecrementCounterInstructionData::try_from_slice(&instruction_data[1..])
+                .map_err(|_| ProgramError::InvalidInstructionData)?;
+            decrement_counter(accounts, instruction_data)
+        }
+        InstructionType::ResetCounter => {
+            let instruction_data = ResetCounterInstructionData::try_from_slice(&instruction_data[1..])
+                .map_err(|_| ProgramError::InvalidInstructionData)?;
+            reset_counter(accounts, instruction_data)
+        }
+        InstructionType::CloseCounter => {
+            let instruction_data = CloseCounterInstructionData::try_from_slice(&instruction_data[1..])
+                .map_err(|_| ProgramError::InvalidInstructionData)?;
+            close_counter(accounts, instruction_data)
+        }
+    }
+}
+
+pub fn create_counter(
+    accounts: &[AccountInfo],
+    instruction_data: CreateCounterInstructionData,
+) -> Result<(), ProgramError> {
+    let signer = accounts.first().ok_or(ProgramError::NotEnoughAccountKeys)?;
+
+    let light_cpi_accounts = CpiAccounts::new(signer, &accounts[1..], LIGHT_CPI_SIGNER);
+
+    let (address, address_seed) = derive_address(
+        &[b"counter", signer.key().as_ref()],
+        &instruction_data
+            .address_tree_info
+            .get_tree_pubkey(&light_cpi_accounts)
+            .map_err(|_| ProgramError::NotEnoughAccountKeys)?,
+        &ID,
+    );
+
+    let new_address_params = instruction_data
+        .address_tree_info
+        .into_new_address_params_packed(address_seed);
+
+    let mut counter = LightAccount::<'_, CounterAccount>::new_init(
+        &ID,
+        Some(address),
+        instruction_data.output_state_tree_index,
+    );
+
+    counter.owner = *signer.key();
+    counter.value = 0;
+
+    let cpi = CpiInputs::new_with_address(
+        instruction_data.proof,
+        vec![counter.to_account_info().map_err(ProgramError::from)?],
+        vec![new_address_params],
+    );
+    cpi.invoke_light_system_program(light_cpi_accounts)
+        .map_err(ProgramError::from)?;
+
+    Ok(())
+}
+
+pub fn increment_counter(
+    accounts: &[AccountInfo],
+    instruction_data: IncrementCounterInstructionData,
+) -> Result<(), ProgramError> {
+    let signer = accounts.first().ok_or(ProgramError::NotEnoughAccountKeys)?;
+
+    let mut counter = LightAccount::<'_, CounterAccount>::new_mut(
+        &ID,
+        &instruction_data.account_meta,
+        CounterAccount {
+            owner: *signer.key(),
+            value: instruction_data.counter_value,
+        },
+    )
+    .map_err(ProgramError::from)?;
+
+    counter.value = counter
+        .value
+        .checked_add(1)
+        .ok_or(CounterError::Overflow)?;
+
+    let light_cpi_accounts = CpiAccounts::new(signer, &accounts[1..], LIGHT_CPI_SIGNER);
+
+    let cpi_inputs = CpiInputs::new(
+        instruction_data.proof,
+        vec![counter.to_account_info().map_err(ProgramError::from)?],
+    );
+    cpi_inputs
+        .invoke_light_system_program(light_cpi_accounts)
+        .map_err(ProgramError::from)?;
+
+    Ok(())
+}
+
+pub fn decrement_counter(
+    accounts: &[AccountInfo],
+    instruction_data: DecrementCounterInstructionData,
+) -> Result<(), ProgramError> {
+    let signer = accounts.first().ok_or(ProgramError::NotEnoughAccountKeys)?;
+
+    let mut counter = LightAccount::<'_, CounterAccount>::new_mut(
+        &ID,
+        &instruction_data.account_meta,
+        CounterAccount {
+            owner: *signer.key(),
+            value: instruction_data.counter_value,
+        },
+    )
+    .map_err(ProgramError::from)?;
+
+    if counter.owner != *signer.key() {
+        return Err(CounterError::Unauthorized.into());
+    }
+
+    counter.value = counter
+        .value
+        .checked_sub(1)
+        .ok_or(CounterError::Underflow)?;
+
+    let light_cpi_accounts = CpiAccounts::new(signer, &accounts[1..], LIGHT_CPI_SIGNER);
+
+    let cpi_inputs = CpiInputs::new(
+        instruction_data.proof,
+        vec![counter.to_account_info().map_err(ProgramError::from)?],
+    );
+
+    cpi_inputs
+        .invoke_light_system_program(light_cpi_accounts)
+        .map_err(ProgramError::from)?;
+
+    Ok(())
+}
+
+pub fn reset_counter(
+    accounts: &[AccountInfo],
+    instruction_data: ResetCounterInstructionData,
+) -> Result<(), ProgramError> {
+    let signer = accounts.first().ok_or(ProgramError::NotEnoughAccountKeys)?;
+
+    let mut counter = LightAccount::<'_, CounterAccount>::new_mut(
+        &ID,
+        &instruction_data.account_meta,
+        CounterAccount {
+            owner: *signer.key(),
+            value: instruction_data.counter_value,
+        },
+    )
+    .map_err(ProgramError::from)?;
+
+    counter.value = 0;
+
+    let light_cpi_accounts = CpiAccounts::new(signer, &accounts[1..], LIGHT_CPI_SIGNER);
+    let cpi_inputs = CpiInputs::new(
+        instruction_data.proof,
+        vec![counter.to_account_info().map_err(ProgramError::from)?],
+    );
+
+    cpi_inputs
+        .invoke_light_system_program(light_cpi_accounts)
+        .map_err(ProgramError::from)?;
+
+    Ok(())
+}
+
+pub fn close_counter(
+    accounts: &[AccountInfo],
+    instruction_data: CloseCounterInstructionData,
+) -> Result<(), ProgramError> {
+    let signer = accounts.first().ok_or(ProgramError::NotEnoughAccountKeys)?;
+
+    let counter = LightAccount::<'_, CounterAccount>::new_close(
+        &ID,
+        &instruction_data.account_meta,
+        CounterAccount {
+            owner: *signer.key(),
+            value: instruction_data.counter_value,
+        },
+    )
+    .map_err(ProgramError::from)?;
+
+    let light_cpi_accounts = CpiAccounts::new(signer, &accounts[1..], LIGHT_CPI_SIGNER);
+
+    let cpi_inputs = CpiInputs::new(
+        instruction_data.proof,
+        vec![counter.to_account_info().map_err(ProgramError::from)?],
+    );
+
+    cpi_inputs
+        .invoke_light_system_program(light_cpi_accounts)
+        .map_err(ProgramError::from)?;
+
+    Ok(())
+}
---
+++ counter/pinocchio/tests/test.rs
@@ -0,0 +1,345 @@
+#![cfg(feature = "test-sbf")]
+
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use counter::{
+    CloseCounterInstructionData, CreateCounterInstructionData, DecrementCounterInstructionData,
+    IncrementCounterInstructionData, ResetCounterInstructionData, CounterAccount,
+};
+use light_client::indexer::CompressedAccount;
+use light_program_test::{
+    program_test::LightProgramTest, AddressWithTree, Indexer, ProgramTestConfig, Rpc, RpcError,
+};
+use light_sdk::address::v1::derive_address;
+use light_sdk::instruction::{
+    account_meta::{CompressedAccountMeta, CompressedAccountMetaClose},
+    PackedAccounts, SystemAccountMetaConfig,
+};
+use solana_sdk::{
+    instruction::Instruction,
+    pubkey::Pubkey,
+    signature::{Keypair, Signer},
+};
+
+#[tokio::test]
+async fn test_counter() {
+    let config = ProgramTestConfig::new(true, Some(vec![("counter", counter::ID.into())]));
+    let mut rpc = LightProgramTest::new(config).await.unwrap();
+    let payer = rpc.get_payer().insecure_clone();
+
+    let address_tree_info = rpc.get_address_tree_v1();
+    let address_tree_pubkey = address_tree_info.tree;
+
+    // Create counter
+    let (address, _) = derive_address(
+        &[b"counter", payer.pubkey().as_ref()],
+        &address_tree_pubkey,
+        &counter::ID.into(),
+    );
+    let merkle_tree_pubkey = rpc.get_random_state_tree_info().unwrap().tree;
+
+    create_counter(
+        &payer,
+        &mut rpc,
+        &merkle_tree_pubkey,
+        address_tree_pubkey,
+        address,
+    )
+    .await
+    .unwrap();
+
+    // Get the created counter
+    let compressed_counter = rpc
+        .get_compressed_account(address, None)
+        .await
+        .unwrap()
+        .value;
+    assert_eq!(compressed_counter.address.unwrap(), address);
+
+    // Test increment
+    increment_counter(&payer, &mut rpc, &compressed_counter)
+        .await
+        .unwrap();
+
+    let compressed_counter = rpc
+        .get_compressed_account(address, None)
+        .await
+        .unwrap()
+        .value;
+
+    // Test decrement
+    decrement_counter(&payer, &mut rpc, &compressed_counter)
+        .await
+        .unwrap();
+
+    let compressed_counter = rpc
+        .get_compressed_account(address, None)
+        .await
+        .unwrap()
+        .value;
+
+    // Test reset
+    reset_counter(&payer, &mut rpc, &compressed_counter)
+        .await
+        .unwrap();
+
+    let compressed_counter = rpc
+        .get_compressed_account(address, None)
+        .await
+        .unwrap()
+        .value;
+
+    // Test close
+    close_counter(&payer, &mut rpc, &compressed_counter)
+        .await
+        .unwrap();
+}
+
+pub async fn create_counter(
+    payer: &Keypair,
+    rpc: &mut LightProgramTest,
+    merkle_tree_pubkey: &Pubkey,
+    address_tree_pubkey: Pubkey,
+    address: [u8; 32],
+) -> Result<(), RpcError> {
+    let system_account_meta_config = SystemAccountMetaConfig::new(counter::ID.into());
+    let mut accounts = PackedAccounts::default();
+    accounts.add_pre_accounts_signer(payer.pubkey());
+    accounts.add_system_accounts(system_account_meta_config);
+
+    let rpc_result = rpc
+        .get_validity_proof(
+            vec![],
+            vec![AddressWithTree {
+                address,
+                tree: address_tree_pubkey,
+            }],
+            None,
+        )
+        .await?
+        .value;
+
+    let output_merkle_tree_index = accounts.insert_or_get(*merkle_tree_pubkey);
+    let packed_address_tree_info = rpc_result.pack_tree_infos(&mut accounts).address_trees[0];
+    let (accounts, _, _) = accounts.to_account_metas();
+
+    let instruction_data = CreateCounterInstructionData {
+        proof: rpc_result.proof,
+        address_tree_info: packed_address_tree_info,
+        output_state_tree_index: output_merkle_tree_index,
+    };
+    let inputs = instruction_data.try_to_vec().unwrap();
+
+    let instruction = Instruction {
+        program_id: counter::ID.into(),
+        accounts,
+        data: [&[counter::InstructionType::CreateCounter as u8][..], &inputs[..]].concat(),
+    };
+
+    rpc.create_and_send_transaction(&[instruction], &payer.pubkey(), &[payer])
+        .await?;
+    Ok(())
+}
+
+pub async fn increment_counter(
+    payer: &Keypair,
+    rpc: &mut LightProgramTest,
+    compressed_account: &CompressedAccount,
+) -> Result<(), RpcError> {
+    let system_account_meta_config = SystemAccountMetaConfig::new(counter::ID.into());
+    let mut accounts = PackedAccounts::default();
+    accounts.add_pre_accounts_signer(payer.pubkey());
+    accounts.add_system_accounts(system_account_meta_config);
+
+    let hash = compressed_account.hash;
+
+    let rpc_result = rpc
+        .get_validity_proof(vec![hash], vec![], None)
+        .await?
+        .value;
+
+    let packed_accounts = rpc_result
+        .pack_tree_infos(&mut accounts)
+        .state_trees
+        .unwrap();
+
+    let counter_account = CounterAccount::deserialize(
+        &mut compressed_account.data.as_ref().unwrap().data.as_slice()
+    ).unwrap();
+
+    let meta = CompressedAccountMeta {
+        tree_info: packed_accounts.packed_tree_infos[0],
+        address: compressed_account.address.unwrap(),
+        output_state_tree_index: packed_accounts.output_tree_index,
+    };
+
+    let (accounts, _, _) = accounts.to_account_metas();
+    let instruction_data = IncrementCounterInstructionData {
+        proof: rpc_result.proof,
+        counter_value: counter_account.value,
+        account_meta: meta,
+    };
+    let inputs = instruction_data.try_to_vec().unwrap();
+
+    let instruction = Instruction {
+        program_id: counter::ID.into(),
+        accounts,
+        data: [&[counter::InstructionType::IncrementCounter as u8][..], &inputs[..]].concat(),
+    };
+
+    rpc.create_and_send_transaction(&[instruction], &payer.pubkey(), &[payer])
+        .await?;
+    Ok(())
+}
+
+pub async fn decrement_counter(
+    payer: &Keypair,
+    rpc: &mut LightProgramTest,
+    compressed_account: &CompressedAccount,
+) -> Result<(), RpcError> {
+    let system_account_meta_config = SystemAccountMetaConfig::new(counter::ID.into());
+    let mut accounts = PackedAccounts::default();
+    accounts.add_pre_accounts_signer(payer.pubkey());
+    accounts.add_system_accounts(system_account_meta_config);
+
+    let hash = compressed_account.hash;
+
+    let rpc_result = rpc
+        .get_validity_proof(vec![hash], vec![], None)
+        .await?
+        .value;
+
+    let packed_accounts = rpc_result
+        .pack_tree_infos(&mut accounts)
+        .state_trees
+        .unwrap();
+
+    let counter_account = CounterAccount::deserialize(
+        &mut compressed_account.data.as_ref().unwrap().data.as_slice()
+    ).unwrap();
+
+    let meta = CompressedAccountMeta {
+        tree_info: packed_accounts.packed_tree_infos[0],
+        address: compressed_account.address.unwrap(),
+        output_state_tree_index: packed_accounts.output_tree_index,
+    };
+
+    let (accounts, _, _) = accounts.to_account_metas();
+    let instruction_data = DecrementCounterInstructionData {
+        proof: rpc_result.proof,
+        counter_value: counter_account.value,
+        account_meta: meta,
+    };
+    let inputs = instruction_data.try_to_vec().unwrap();
+
+    let instruction = Instruction {
+        program_id: counter::ID.into(),
+        accounts,
+        data: [&[counter::InstructionType::DecrementCounter as u8][..], &inputs[..]].concat(),
+    };
+
+    rpc.create_and_send_transaction(&[instruction], &payer.pubkey(), &[payer])
+        .await?;
+    Ok(())
+}
+
+pub async fn reset_counter(
+    payer: &Keypair,
+    rpc: &mut LightProgramTest,
+    compressed_account: &CompressedAccount,
+) -> Result<(), RpcError> {
+    let system_account_meta_config = SystemAccountMetaConfig::new(counter::ID.into());
+    let mut accounts = PackedAccounts::default();
+    accounts.add_pre_accounts_signer(payer.pubkey());
+    accounts.add_system_accounts(system_account_meta_config);
+
+    let hash = compressed_account.hash;
+
+    let rpc_result = rpc
+        .get_validity_proof(vec![hash], vec![], None)
+        .await?
+        .value;
+
+    let packed_accounts = rpc_result
+        .pack_tree_infos(&mut accounts)
+        .state_trees
+        .unwrap();
+
+    let counter_account = CounterAccount::deserialize(
+        &mut compressed_account.data.as_ref().unwrap().data.as_slice()
+    ).unwrap();
+
+    let meta = CompressedAccountMeta {
+        tree_info: packed_accounts.packed_tree_infos[0],
+        address: compressed_account.address.unwrap(),
+        output_state_tree_index: packed_accounts.output_tree_index,
+    };
+
+    let (accounts, _, _) = accounts.to_account_metas();
+    let instruction_data = ResetCounterInstructionData {
+        proof: rpc_result.proof,
+        counter_value: counter_account.value,
+        account_meta: meta,
+    };
+    let inputs = instruction_data.try_to_vec().unwrap();
+
+    let instruction = Instruction {
+        program_id: counter::ID.into(),
+        accounts,
+        data: [&[counter::InstructionType::ResetCounter as u8][..], &inputs[..]].concat(),
+    };
+
+    rpc.create_and_send_transaction(&[instruction], &payer.pubkey(), &[payer])
+        .await?;
+    Ok(())
+}
+
+pub async fn close_counter(
+    payer: &Keypair,
+    rpc: &mut LightProgramTest,
+    compressed_account: &CompressedAccount,
+) -> Result<(), RpcError> {
+    let system_account_meta_config = SystemAccountMetaConfig::new(counter::ID.into());
+    let mut accounts = PackedAccounts::default();
+    accounts.add_pre_accounts_signer(payer.pubkey());
+    accounts.add_system_accounts(system_account_meta_config);
+
+    let hash = compressed_account.hash;
+
+    let rpc_result = rpc
+        .get_validity_proof(vec![hash], vec![], None)
+        .await?
+        .value;
+
+    let packed_accounts = rpc_result
+        .pack_tree_infos(&mut accounts)
+        .state_trees
+        .unwrap();
+
+    let counter_account = CounterAccount::deserialize(
+        &mut compressed_account.data.as_ref().unwrap().data.as_slice()
+    ).unwrap();
+
+    let meta_close = CompressedAccountMetaClose {
+        tree_info: packed_accounts.packed_tree_infos[0],
+        address: compressed_account.address.unwrap(),
+    };
+
+    let (accounts, _, _) = accounts.to_account_metas();
+    let instruction_data = CloseCounterInstructionData {
+        proof: rpc_result.proof,
+        counter_value: counter_account.value,
+        account_meta: meta_close,
+    };
+    let inputs = instruction_data.try_to_vec().unwrap();
+
+    let instruction = Instruction {
+        program_id: counter::ID.into(),
+        accounts,
+        data: [&[counter::InstructionType::CloseCounter as u8][..], &inputs[..]].concat(),
+    };
+
+    rpc.create_and_send_transaction(&[instruction], &payer.pubkey(), &[payer])
+        .await?;
+    Ok(())
+}
