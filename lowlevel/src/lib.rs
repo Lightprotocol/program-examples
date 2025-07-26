06eb9629b05b4135b356bcbeaf5e72e05657e5d3
ananas <jorrit@lightprotocol.com>
2025-07-26 01:33:44 +0100

    feat: add lowlevel example

---
+++ lowlevel/src/lib.rs
@@ -0,0 +1,87 @@
+#![allow(unexpected_cfgs)]
+
+use anchor_lang::{prelude::*, AnchorDeserialize, AnchorSerialize};
+use light_compressed_account::instruction_data::with_account_info::{
+    CompressedAccountInfo, OutAccountInfo,
+};
+use light_sdk::{
+    address::v1::derive_address,
+    cpi::{CpiAccounts, CpiInputs, CpiSigner},
+    derive_light_cpi_signer,
+    instruction::{PackedAddressTreeInfo, ValidityProof},
+};
+declare_id!("HNqStLMpNuNJqhBF1FbGTKHEFbBLJmq8RdJJmZKWz6jH");
+
+pub const LIGHT_CPI_SIGNER: CpiSigner =
+    derive_light_cpi_signer!("HNqStLMpNuNJqhBF1FbGTKHEFbBLJmq8RdJJmZKWz6jH");
+
+pub const FIRST_SEED: &[u8] = b"first";
+pub const SECOND_SEED: &[u8] = b"second";
+
+#[program]
+pub mod lowlevel {
+
+    use super::*;
+
+    /// Creates a new compressed account with initial data
+    pub fn create_address_and_output_without_address<'info>(
+        ctx: Context<'_, '_, '_, 'info, GenericAnchorAccounts<'info>>,
+        proof: ValidityProof, // Required for the address create, it proves that the address does not exist yet in the light address tree.
+        address_tree_info: PackedAddressTreeInfo,
+        output_state_tree_index: u8,
+        encrypted_utxo: Vec<u8>,    // must be checked by your zkp
+        output_utxo_hash: [u8; 32], // must be checked by your zkp
+    ) -> Result<()> {
+        let light_cpi_accounts = CpiAccounts::new(
+            ctx.accounts.signer.as_ref(),
+            ctx.remaining_accounts,
+            crate::LIGHT_CPI_SIGNER,
+        );
+        let address_pubkey = address_tree_info
+            .get_tree_pubkey(&light_cpi_accounts)
+            .map_err(|_| ErrorCode::AccountNotEnoughKeys)?;
+
+        if address_pubkey.to_bytes() != light_sdk::constants::ADDRESS_TREE_V1 {
+            msg!("Invalid address tree");
+            return Err(ProgramError::InvalidAccountData.into());
+        }
+
+        let (_address, address_seed) = derive_address(
+            &[FIRST_SEED, ctx.accounts.signer.key().as_ref()],
+            &address_pubkey,
+            &crate::ID,
+        );
+
+        let output_account = CompressedAccountInfo {
+            address: None,
+            input: None,
+            output: Some(OutAccountInfo {
+                discriminator: 1u64.to_le_bytes(), // doesn't really matter as long as you only have one type of compressed account.
+                output_merkle_tree_index: output_state_tree_index,
+                lamports: 0,
+                data: encrypted_utxo,
+                data_hash: output_utxo_hash,
+            }),
+        };
+
+        // Create 1 output compressed account without address
+        // Create 1 address without compressed account
+        let cpi_inputs = CpiInputs::new_with_address(
+            proof,
+            vec![output_account],
+            vec![address_tree_info.into_new_address_params_packed(address_seed)],
+        );
+        msg!("cpi_inputs {:?}", cpi_inputs);
+        cpi_inputs
+            .invoke_light_system_program(light_cpi_accounts)
+            .map_err(ProgramError::from)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct GenericAnchorAccounts<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
