0f1f93dc9b776b8e95570bb6a092c5dc71f0b463
Swenschaeferjohann <swen@lightprotocol.com>
2025-11-19 22:19:47 -0500

    clean

--- create-and-update/programs/create-and-update/src/lib.rs
+++ create-and-update/programs/create-and-update/src/lib.rs
@@ -260,9 +260,7 @@ pub struct GenericAnchorAccounts<'info> {
     pub signer: Signer<'info>,
 }
 
-// #[derive(Debug, Default, BorshSerialize, BorshDeserialize, LightDiscriminator, LightHasher)]
 #[derive(Clone, LightDiscriminator, Default, AnchorDeserialize, AnchorSerialize)]
-// #[account]
 pub struct DataAccount {
     pub owner: Pubkey,
     pub message: String,
